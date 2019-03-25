f5b02f8d30e5b7902093ff3fa96ffdd0e7eddfbd
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2019-03-25 22:39:48 +0530

    modified script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 setTimeout(function(){
     $('h2').addClass("animated bounceIn");
     $('h3').addClass("animate bounceIn");
+    $('#container').addClass("animated fadeIn");
+    $('h1').addClass("animated bounceIn");
 } , 500);
 
