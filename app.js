d70eafe49d655f5120f7c5227843664c386831ef
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2019-07-28 21:36:27 +0530

    Added all

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 1200) {
+        $('.gotopbtn').fadeIn();
+    } else {
+        $('.gotopbtn').fadeOut();
+    }
+
+});
+$(".maintext").html('The <a href="https://www.amazon.com/Samsung-Factory-Unlocked-Warranty-Ceramic/dp/B07NPJ3G2N/ref=sr_1_1_sspa?crid=3LTPYDX5PW9X0&keywords=s10&qid=1563094815&s=gateway&sprefix=s10%2Caps%2C481&sr=8-1-spons&th=1" target="_blank">Samsung Galaxy S10 mobile</a> features a 6.1" (15.49 cm) display with a screen resolution of 1440 x 3040 pixels and runs on Android v9.0 (Pie) operating system. The device is powered by Octa core (2.73 GHz, Dual core, M4 Mongoose + 2.31 GHz, Dual core, Cortex A75 + 1.95 GHz, Quad core, Cortex A55) processor paired with 8 GB of RAM. As far as the battery is concerned it has 3400 mAh. Over that, as far as the rear camera is concerned this mobile has a 12MP + 12MP + 16MP camera . Other sensors include Light sensor, Proximity sensor, Accelerometer, Barometer, Compass, Gyroscope. So, does it have a fingerprint sensor? Yes, it does. For graphical performance that can make games run smoothly, this phone has got a Mali-G76 MP12 GPU. On board storage is at 128 GB with the option to expand the memory by Yes Up to 512 GB. Design is one of the most important factors when it comes to mobiles. This phone is 7.8 mm slim and weighs 157 grams. you can get from <a href="bonus-pdf-26-point-seo-checklist-for-new-websites-seo-audits.pdf" download="myPDF.pdf" target="_blank">here</a>');
+$(".uppertext").html('Now that we’ve got a next-gen handheld, I knew it wouldn’t be long before we started seeing commentary about the imminent “death” of the 3DS, as it would seem there’s no longer a place for it in the paradigm. But let’s not count it out just yet — because the Switch still has some climbing to do before it can replace the incumbent.<ul><li>Samsung</li><li>Motorola</li><li>Redmi</li></ul>');
