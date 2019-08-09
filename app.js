dca18b41b3eef83608846f3a7ed55448b833fe83
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2019-08-09 22:45:40 +0530

    updated

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+document.addEventListener("mouseleave", function(e){
+    if( e.clientY < 0 )
+    {
+//         alert("Hey don't leave. I have an free eBook for you");
+        $('#exampleModalCenter').modal("show");
+    }
+}, false);
 $(document).scroll(function () {
     var y = $(this).scrollTop();
     if (y > 1200) {
