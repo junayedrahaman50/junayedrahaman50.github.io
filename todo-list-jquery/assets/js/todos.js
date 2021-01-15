721ec165391379120c46e1c0e2bfafe59c58801c
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2021-01-16 00:14:51 +0530

    Added Jquery todo

---
+++ todo-list-jquery/assets/js/todos.js
@@ -0,0 +1,32 @@
+$('ul').on('click','li', function(){
+$(this).toggleClass('change');
+});
+//Delete todos
+$('ul').on('click','span', function(event){
+    $(this).parent().fadeOut(500 , function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+var textItem = $("input[type = 'text']");
+textItem.on('keypress',function (event) {
+    if($(this).val() != ""){
+    var inputText = $(this).val();
+    if(event.which === 13){
+    $(this).val("");
+	$('ul').append("<li><span><i class='fas fa-trash'></i></span>  " + inputText + "</li>"); 
+    }  
+  }
+    else{
+        if(event.which === 13){
+         alert('You must enter something');
+        }
+    }
+});
+$('.fa-pencil-alt').on('click',function(){
+   textItem.fadeToggle(100); 
+});
+
+
+
+
