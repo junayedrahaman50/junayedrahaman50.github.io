01044817bbd4080c3bad5e1bfba8c4cea0b27499
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2021-01-16 00:35:31 +0530

    Added Jquery todo

--- app2.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("mouseleave", function(e){
-    if( e.clientY < 0 )
-    {
-//         alert("Hey don't leave. I have an free eBook for you");
-        $('#exampleModalCenter').modal("show");
-    }
-}, false);
