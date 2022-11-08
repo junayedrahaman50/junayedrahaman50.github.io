e9d5a7962c28821265d3b18f13f3f865c26478b3
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2022-11-08 12:32:04 +0530

    responsiveness achieved

--- QuickBooks/js/main.js
+++ QuickBooks/js/main.js
@@ -1,3 +1,5 @@
+// Google Map
+
 let map;
 
 function initMap() {
@@ -8,3 +10,32 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
